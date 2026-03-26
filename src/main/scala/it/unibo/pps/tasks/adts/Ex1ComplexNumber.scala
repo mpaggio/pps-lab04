2a4a7872f32a530d4464b15c97b676bf6de91c1e
Marco Paggetti <marco.paggetti@studio.unibo.it>
2026-03-26 10:28:58 +0100

    Basic complex number

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -5,7 +5,7 @@ package it.unibo.pps.tasks.adts
  *  the test in ComplexTest.
  */
 
-object Ex1ComplexNumbers:
+object Ex1ComplexNumber:
 
   trait ComplexADT:
     type Complex
@@ -18,13 +18,21 @@ object Ex1ComplexNumbers:
       def asString(): String
 
   object BasicComplexADT extends ComplexADT:
-
+    case class ComplexNumber(re: Double, im: Double)
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Complex = ComplexNumber
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex match
+        case ComplexNumber(r, i) => r
+      def im(): Double = complex match
+        case ComplexNumber(r, i) => i
+      def sum(other: Complex): Complex = (complex, other) match
+        case (ComplexNumber(r1, i1), ComplexNumber(r2, i2)) => ComplexNumber(r1 + r2, i1 + i2)
+      def subtract(other: Complex): Complex = (complex, other) match
+        case (ComplexNumber(r1, i1), ComplexNumber(r2, i2)) => ComplexNumber(r1 - r2, i1 - i2)
+      def asString(): String = complex match
+        case ComplexNumber(r, i) if i == 0 => s"$r"
+        case ComplexNumber(r, i) if r == 0 => s"${i}i"
+        case ComplexNumber(r, i) if i > 0 => s"$r + ${i}i"
+        case ComplexNumber(r, i) => s"$r - ${-i}i"
--- src/test/scala/it/unibo/pps/tasks/adts/ComplexNumberTests.scala
+++ src/test/scala/it/unibo/pps/tasks/adts/ComplexNumberTests.scala
@@ -2,12 +2,12 @@ package it.unibo.pps.tasks.adts
 
 import org.junit.*
 import org.junit.Assert.*
-import Ex1ComplexNumbers.*
+import Ex1ComplexNumber.*
 
 /* Tests should be clear, but note they are expressed independently of the 
    specific implementation  -- UNCOMMENT FOR THE EXERCISE!
 */
-class ComplexTest:
+class ComplexNumberTests:
 
   // Choice of implementation to test
   val complexADT: ComplexADT = BasicComplexADT
@@ -15,22 +15,22 @@ class ComplexTest:
 
   // From now, everything is independent of specific implementation of Complex
 
-  @Test def testReal() =
+  @Test def testReal(): Unit =
     assertEquals(10, complex(10, 20).re(), 0)
 
-  @Test def testImaginary() =
+  @Test def testImaginary(): Unit =
     assertEquals(20, complex(10, 20).im(), 0)
 
-  @Test def testSum() =
+  @Test def testSum(): Unit =
     assertEquals(complex(11, 22), complex(10, 20) sum complex(1, 2))
 
-  @Test def testSubtract() =
+  @Test def testSubtract(): Unit =
     assertEquals(complex(9, 18), complex(10, 20) subtract complex(1, 2))
 
-  @Test def testAsString() =
+  @Test def testAsString(): Unit =
     assertEquals("10.0 + 5.0i", complex(10.0, 5.0).asString())
 
-  @Test def optionalTestAdvancedAsString() =
+  @Test def optionalTestAdvancedAsString(): Unit =
     assertEquals("0.0", complex(0, 0).asString())
     assertEquals("10.0", complex(10.0, 0).asString())
     assertEquals("10.0 + 5.0i", complex(10.0, 5.0).asString())
